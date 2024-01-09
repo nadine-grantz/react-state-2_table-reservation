35e28c5b0ded8047816b0c9dec563857fd2a3d7d
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-09 17:02:55 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleAdding() {
+    setPeople(people + 1);
+  }
+
+  function handleDelete() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onAdd={handleAdding} onDelete={handleDelete} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
